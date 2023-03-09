d9d4432088d67adb555bee5f12272ceada66e6d3
Tom Sharkey <tbsharkey@gmail.com>
2023-03-08 22:20:03 -0800

    Added two questions to the JS file

--- README.md
+++ README.md
@@ -4,7 +4,48 @@
 
 I built a simple webpage that includes biographical information on myself and asks the user a series of questions. The user is greeted, asked the questions, and then thanked for participating.
 
-### Author: Student/Group Name
+### Author: Tom Sharkey
+
+### Links and Resources
+
+* [Github Repo](https://github.com/tbsharkey/About-Me)
+
+### Lighthouse Accessibility Report Score
+
+![Lighthouse report](/img/lighthouse-report.png)
+
+### Reflections and Comments
+
+This was a good exercise with Javascript reinforcing the basic concept of if statements. 
+
+# LAB - 03
+
+## About Me
+
+Continuning on with this same repo I will be adding additional content for the third lab. The following user stories need to be implemented:
+
+As a user, I would like to view a series of data related to the site owners interest so that the I can quickly view more information about them.
+Create a “Top Ten” at the bottom of your HTML page as an ordered list in HTML. Some ideas that you can include could be top ten movies, top ten favorite places, or top ten places to visit. You may choose to do whatever top ten list that you wish.
+Convert your work experience and education summary into an unordered list using HTML
+As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
+Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+Indicates through an alert if the guess is “too high” or “too low”.
+It should give the user exactly four opportunities to get the correct answer.
+After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+Add a 7th question that has multiple possible correct answers that are stored in an array.
+Give the user 6 attempts to guess the correct answer.
+The guesses will end once the user guesses a correct answer or they run out of attempts.
+Display all the possible correct answers to the user.
+Consider using a loop of some sort for this question.
+As a user, I would like to know my final score so that I can know how well I did.
+Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
+Using Lighthouse in the Chrome DevTools, analyze the accessibility of your application.
+
+Strive for a score between 50-65. Make necessary adjustments based on the report to achieve that score.
+Add a screenshot of your score to your README.md file.
+
+### Author: Tom Sharkey
 
 ### Links and Resources
 
--- js/app.js
+++ js/app.js
@@ -1,56 +1,111 @@
 'use strict';
 
+let score = 0
 let name = prompt("Hello, what is your name?");
 alert(`Hello ${name}, It's nice to meet you. I have a game to play with you. You will be answering a series of questions about my background.`);
 
 let answer1 = prompt("Was I born in Missouri?");
 answer1 = answer1.toUpperCase();
 if (answer1 === "YES") {
-//  console.log("Yes, that's correct.") 
+  //  console.log("Yes, that's correct."); 
   alert("Yes, that's correct.");
+  score++;
 } else {
-// console.log("That's incorrect, I am from Virginia originally.");
+  // console.log("That's incorrect, I am from Virginia originally.");
   alert("That's incorrect, I am from Virginia originally.");
 };
 
 let answer2 = prompt("Do I have more than one AWS certification?");
 answer2 = answer2.toUpperCase();
 if (answer2 === "NO") {
-// console.log("Yes, that's correct.");
+  // console.log("Yes, that's correct.");
   alert("Yes, that's correct.");
+  score++;
 } else {
-//  console.log("That's incorrect, though I wish otherwise at the moment I only have one.");  
+  //  console.log("That's incorrect, though I wish otherwise at the moment I only have one.");  
   alert("That's incorrect, though I wish otherwise at the moment I only have one.");
 };
 
 let answer3 = prompt("Did I work in the news media previously?");
 answer3 = answer3.toUpperCase();
 if (answer3 === "NO") {
-//    console.log("Yes, that's correct.");
+  //    console.log("Yes, that's correct.");
   alert("Yes, that's correct.");
+  score++;
 } else {
-// console.log("That's incorrect and I'm thankful it is.");  
+  // console.log("That's incorrect and I'm thankful it is.");  
   alert("That's incorrect and I'm thankful it is.");
 };
 
 let answer4 = prompt("Did I serve in the US Navy?");
 answer4 = answer4.toUpperCase();
 if (answer4 === "NO") {
- // console.log("Yes, that's correct.");   
+  // console.log("Yes, that's correct.");   
   alert("Yes, that's correct.");
+  score++;
 } else {
-// console.log("No, this may be a technicality but I'm not going to agree with you.");
+  // console.log("No, this may be a technicality but I'm not going to agree with you.");
   alert("No, this may be a technicality but I'm not going to agree with you.");
 };
 
 let answer5 = prompt("Do I have any cloud certifications?");
 answer5 = answer5.toUpperCase();
 if (answer5 === "YES") {
-// console.log("Yes, that's correct as I have two.");
+  // console.log("Yes, that's correct as I have two.");
   alert("Yes, that's correct as I have two.");
+  score++;
 } else {
-//    console.log("That's incorrect, give me some credit here!");
+  //    console.log("That's incorrect, give me some credit here!");
   alert("That's incorrect, give me some credit here!");
 };
 
-alert(`It was a pleasure to play this game with you ${name}. I hope you have a great day!`)
+// Question 6 which starts the third lab
+alert(`Ok ${name}, now it's time for a guessing game.`);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+let counter = 0;
+while (counter <= 4) {
+  let guess = prompt("Enter a number between 1-10.");
+
+  if (Number(guess) === randomNumber) {
+    alert('Congratulations! You have guessed the correct number!');
+    score++;
+    break;
+  }
+
+  counter++;
+  if (guess > randomNumber) {
+    alert("Too high, please try again.");
+  } else {
+    alert("Too low, please try again");
+  }
+}
+
+if (counter > 4) {
+  alert(`Sorry, you didn't guess the right number within four chances. The number is ${randomNumber}.`);
+}
+
+// Question 7
+alert(`Ok ${name}, now it's time for the last guessing game.`);
+
+const answers = [
+  "Bellingham",
+  "Portland",
+  "Santa Cruz",
+];
+
+let cityCounter = 0;
+while (cityCounter < 6) {
+  let guess = prompt("Name one of my three favorite cities.");
+  if (guess === "Bellingham" || guess === "Portland" || guess === "Santa Cruz") {
+    alert("That's correct! It's one of my top three cities!")
+    score++;
+    break;
+  } else {
+    alert("That's not right.")
+    cityCounter++
+  }
+}
+if (cityCounter === 6) {
+  alert("Sorry you are out of chances.")
+}
+alert(`It was a pleasure to play this game with you ${name}. You scored ${score} points! I hope you have a great day!`)
